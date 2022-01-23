00f49357ea3ba94c007c3c5c53dc0ed3a158ddc3
Carlos Wagner <carlos.sperling@granter.com.br>
2022-01-23 09:36:27 -0300

    Criado método de atualizar produtos e classe ApiConfig.

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -34,20 +34,8 @@ namespace DevIO.Api
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddAutoMapper(typeof(Startup));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.SuppressModelStateInvalidFilter = true;
-            });
 
-            services.AddCors(options => {
-                options.AddPolicy("Development",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-            });
-
+            services.WebApiConfig();
             services.ResolveDependencies();
         }
 
@@ -63,10 +51,7 @@ namespace DevIO.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseCors("Development");
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvcConfiguration();
         }
     }
 }
